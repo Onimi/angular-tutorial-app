0a45dc87c829c258beb275e586a8e846dc9387d3
Aleksandr Y. Kolodyazhnyi <aleksandr.kolodyazhnyi@ssa.group>
2018-10-09 18:04:19 +0300

    more readable code for tutorials/hometask-6.ts

--- tutorials/hometask-6.ts
+++ tutorials/hometask-6.ts
@@ -1,3 +1,4 @@
+// types:
 type Salaries = {
     [key: string]: number
 }
@@ -10,26 +11,64 @@ type ClassListHolder = {
     className: string
 }
 
+
+// test data:
+const SalaryList: Salaries[] = [
+    {},
+    {
+        "Adam": 100,
+        "Brayan": 200,
+        "Chad": 270
+    },
+    {
+        "David": 300,
+        "Ed": 280,
+        "Frank": 350,
+        "George": 1000,
+        "Henry": 420
+    }
+];
+
+const SNOList: StringNumberObject[] = [
+    {
+        width: 400,
+        heigth: 600,
+        title: 'Main menu'
+    },
+    {
+        top: 20,
+        bottom: 15,
+        text: 'Hello everybody',
+        right: 25,
+        left: 25,
+        alt: 'Default message'
+    }
+];
+
+const CountDataList: {base: number, count: number}[] = [
+    {base: 1, count: 10},
+    {base: 2, count: 5},
+    {base: 3.5, count: 7},
+];
+
+const CLHList: ClassListHolder[] = [
+    { className: 'open menu' },
+    { className: 'btn new hidden' },
+    { className: 'new menu' }
+];
+const ClassList: string[] = [
+    'new', 'hidden', 'open',  'disabled'
+]
+
+// FUNCTIONS:
 // sum of salaries:
 function sumSalary(salaries: Salaries): number {
     return Object.keys(salaries).reduce( (sum: number, key: string) => sum + salaries[key], 0 );
 }
 
-const sal0 = {};
-const sal1 = {
-    "Adam": 100,
-    "Brayan": 200,
-    "Chad": 270
-}
-
-console.log(sumSalary(sal0));
-console.log(sumSalary(sal1));
-console.log('---\n');
-
-
 // max salary owner:
 function ownerOfMaxSalary(salaries: Salaries): string {
-    const DEFAULT: string = "There is no emploees";
+    const DEFAULT: string = "There is no employees";
     const employees: string[] = Object.keys(salaries);
     if (!employees.length) {
         return DEFAULT;
@@ -37,35 +76,19 @@ function ownerOfMaxSalary(salaries: Salaries): string {
     return Object.keys(salaries).reduce( (owner: string, key: string) => salaries[key] > salaries[owner] ? key : owner );
 }
 
-console.log(ownerOfMaxSalary(sal0));
-console.log(ownerOfMaxSalary(sal1));
-console.log('---\n');
-
-
 // double numeric properties:
-function isNumeric(n: number | string): boolean {
-    return !isNaN( parseFloat( <string>n ) ) && isFinite( <number>n );
-}
-
 function doubleNumeric(arg: StringNumberObject): void {
+
+    function isNumeric(n: number | string): boolean {
+        return !isNaN( parseFloat( <string>n ) ) && isFinite( <number>n );
+    }
+
     Object.keys(arg).forEach( (key: string) => {
         if ( isNumeric( arg[key] ) ) {
             arg[key] = <number>arg[key] * 2;
         }
     } );
 }
-
-const menu0 = {
-    width: 400,
-    heigth: 600,
-    title: 'Main menu'
-}
-
-doubleNumeric(menu0);
-
-console.log(menu0);
-console.log('---\n');
-
 
 // list of multiples:
 function countBy(base: number, count: number): number[] {
@@ -82,12 +105,6 @@ function countBy(base: number, count: number): number[] {
     return times;
 }
 
-console.log( countBy(1, 10) );
-console.log( countBy(2, 5) );
-console.log( countBy(3.5, 4) );
-console.log('---\n');
-
-
 // add class to class list:
 function addClass(obj: ClassListHolder, className: string): void {
     const list: string[] = obj.className.split(' ');
@@ -103,27 +120,68 @@ function addClassAlt(obj: ClassListHolder, className: string): void {
     }
 }
 
-const classListHolder = {
-    className: 'open menu'
-}
-const classListHolderAlt = {
-    className: 'open menu'
-}
+// TESTING:
+console.log('\n');
 
-console.log(classListHolder);
-addClass(classListHolder, 'new');
-console.log(classListHolder);
-addClass(classListHolder, 'open');
-console.log(classListHolder);
-addClass(classListHolder, 'hidden');
-console.log(classListHolder);
+// sum of salaries:
+console.log('sum of salaries:');
+SalaryList.forEach( data => {
+    console.log( 'Input:', data );
+    console.log( 'Result:', sumSalary(data) );
+    console.log('-\n');
+} );
 console.log('---\n');
 
-console.log(classListHolderAlt);
-addClassAlt(classListHolderAlt, 'new');
-console.log(classListHolderAlt);
-addClassAlt(classListHolderAlt, 'open');
-console.log(classListHolderAlt);
-addClassAlt(classListHolderAlt, 'hidden');
-console.log(classListHolderAlt);
+// max salary owner:
+console.log('max salary owner:');
+SalaryList.forEach( data => {
+    console.log( 'Input:', data );
+    console.log( 'Result:', ownerOfMaxSalary(data) );
+    console.log('-\n');
+} );
+console.log('---\n');
+
+// double numeric properties:
+console.log('double numeric properties:');
+SNOList.forEach(data => {
+    console.log( 'input:', data );
+    doubleNumeric(data)
+    console.log( 'Result:', data );
+    console.log('-\n');
+});
+console.log('---\n');
+
+// list of multiples:
+console.log('list of multiples:');
+CountDataList.forEach(data => {
+    console.log( 'input:', `${data.base} ${data.count}` );
+    console.log( 'Result:', countBy(data.base, data.count) );
+    console.log('-\n');
+});
+console.log('---\n');
+
+// add class to class list:
+console.log('add class to class list:');
+CLHList.forEach( item => {
+    const data: ClassListHolder = Object.assign( {}, item );
+    ClassList.forEach ( className => {
+        console.log( 'input:', data, `'${className}'` );
+        addClass( data, className )
+        console.log( 'Result:', data );
+        console.log('-\n');
+    } );
+    console.log('\n');
+} );
+console.log('---\n');
+
+CLHList.forEach( item => {
+    const data: ClassListHolder = Object.assign( {}, item );
+    ClassList.forEach ( className => {
+        console.log( 'input:', data, `'${className}'` );
+        addClassAlt( data, className )
+        console.log( 'Result:', data );
+        console.log('-\n');
+    } );
+    console.log('\n');
+} );
 console.log('---\n');
